24733cee7ac60bc57f7b5168a4bb3c9952bde4a5
EdnaK18 <reginakwanrx@gmail.com>
2022-11-22 13:38:17 -0500

    fix router bug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Route path="/Login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+    <>
+      <Link to='/'>Login</Link>
+      <Link to='/Dashboard'>Dashboard</Link>
       {/* if user is logged in, show logout button */}
-    </nav>
+   </>
   );
 };
 
 export default Navbar;
+
