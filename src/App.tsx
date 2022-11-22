b7855faaee78235ee29aa4db84bfe1caa3b93712
EdnaK18 <reginakwanrx@gmail.com>
2022-11-22 12:56:49 -0500

    fix route error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import "./stylesheets/app.css";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
 const Navbar = () => {
   return (
     <nav>
-      <Route path="/Login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Link to='/'>Login</Link>
+      <Link to="/dashboard">Dashboard</Link>
       {/* if user is logged in, show logout button */}
     </nav>
   );
