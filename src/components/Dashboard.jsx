082c0eb1835ce2e8aaa9c7d7bece2bddbe2dcd66
EdnaK18 <reginakwanrx@gmail.com>
2022-11-24 10:23:27 -0500

    add comments for functions to add

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -15,7 +15,13 @@ const Dashboard = () => {
 
   const sampleTimeLog = [];
 
+//add a function to fetch user activities and resources (call this function after adding and activity, logging time, or adding a resource and trigger re-render)
 
+//add function to add a resource
+
+//add a function to add an activity
+
+// add a function to log time
 
   return (
     <div className="dashboard">
